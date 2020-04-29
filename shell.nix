e5cc3bc565d68159b8bace26baebebc9856a8c05
Daniël de Kok <me@danieldk.eu>
2020-04-29 09:56:49 +0200

    Add scaffolding

---
+++ shell.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> {};
+
+let
+  sources = import ./nix/sources.nix;
+  mozilla = callPackage "${sources.mozilla}/package-set.nix" {};
+  rust = mozilla.rustChannelOf { channel = "nightly"; date = "2020-04-01"; };
+in mkShell {
+  nativeBuildInputs = [
+    maturin
+    python3
+    rust.rust
+  ];
+}
