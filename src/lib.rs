ef3fc12b42956b56df1029e2cde28fadd1974834
Daniël de Kok <me@danieldk.eu>
2020-04-29 12:09:52 +0200

    Add basic Sentence type

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use pyo3::prelude::*;
 
+mod sentence;
+pub use sentence::{PySentence, PySentenceIterator, PyToken};
+
+pub(crate) mod util;
+
 #[pymodule]
-fn sticker2(_py: Python, _m: &PyModule) -> PyResult<()> {
-    Ok(())
+fn sticker2(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PySentence>()
 }
