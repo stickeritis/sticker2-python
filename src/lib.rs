302deac9730b3b750a2f2a67e97c1bd7041ec856
Daniël de Kok <me@danieldk.eu>
2020-05-22 12:01:22 +0200

    Add `Annotator` class

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,21 @@ use pyo3::prelude::*;
 mod config;
 pub use config::{PyConfig, PyLabeler, PyModel};
 
+pub(crate) mod io;
+
 mod sentence;
 pub use sentence::{PySentence, PySentenceIterator, PyToken};
 
+mod annotator;
+pub use annotator::PyAnnotator;
+
 #[pymodule]
 fn sticker2(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PyAnnotator>()?;
     m.add_class::<PyConfig>()?;
     m.add_class::<PyLabeler>()?;
     m.add_class::<PyModel>()?;
-    m.add_class::<PySentence>()
+    m.add_class::<PySentence>()?;
+
+    Ok(())
 }
