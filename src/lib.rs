5fa4cba2ae6bb085a98478bfb7a11eb367f1408e
Daniël de Kok <me@danieldk.eu>
2020-05-22 09:35:36 +0200

    Add basic support for loading configurations

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
 use pyo3::prelude::*;
 
+mod config;
+pub use config::{PyConfig, PyLabeler, PyModel};
+
 mod sentence;
 pub use sentence::{PySentence, PySentenceIterator, PyToken};
 
 #[pymodule]
 fn sticker2(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PyConfig>()?;
+    m.add_class::<PyLabeler>()?;
+    m.add_class::<PyModel>()?;
     m.add_class::<PySentence>()
 }
